e39b33117dd8cb9b449bc8ec3a8256d72e1f359c
Hamza Remmal <hamza@remmal.net>
2025-04-06 23:30:28 +0200

    t

--- DataflowRewriter/Rewrites/JoinRewriteCorrect.lean
+++ DataflowRewriter/Rewrites/JoinRewriteCorrect.lean
@@ -29,8 +29,6 @@ namespace DataflowRewriter.JoinRewrite
 
 open StringModule
 
-set_option maxHeartbeats 0
-
 attribute [drcompute] Batteries.AssocList.toList Function.uncurry Module.mapIdent List.toAssocList List.foldl Option.pure_def Option.bind_eq_bind Option.bind_some Module.renamePorts Batteries.AssocList.mapKey InternalPort.map toString Nat.repr Nat.toDigits Nat.toDigitsCore Nat.digitChar List.asString Option.bind Batteries.AssocList.mapVal Batteries.AssocList.eraseAll Batteries.AssocList.eraseP beq_self_eq_true Option.getD cond beq_self_eq_true  beq_iff_eq  InternalPort.mk.injEq  String.reduceEq  and_false  imp_self BEq.beq AssocList.bijectivePortRenaming AssocList.keysList AssocList.eraseAllP List.inter
 
 attribute [drdecide] InternalPort.mk.injEq and_false decide_False decide_True and_true Batteries.AssocList.eraseAllP  InternalPort.mk.injEq
@@ -115,10 +113,13 @@ variable (T₁ T₂ T₃) in
     unfold Module.connect''
     dsimp
 
+#check (lhsModule T₁ T₂ T₃).internals
+
+set_option maxHeartbeats 0 in
 variable (T₁ T₂ T₃) in
 def rhsModuleType : Type := by
   precomputeTac [T| (rewriteLhsRhs S₁ S₂ S₃).output_expr, @environmentRhs T₁ T₂ T₃ S₁ S₂ S₃ ] by
-    simp [drunfold,seval,drcompute,drdecide,-AssocList.find?_eq]
+    simp [drunfold,seval,drcompute,drdecide,-AssocList.find?_eq,-PortMap.cons_find?,-PortMap.getIO_cons]
 
 variable (T₁ T₂ T₃) in
 @[drunfold] def rhsModule : StringModule (rhsModuleType T₁ T₂ T₃) := by
@@ -183,17 +184,321 @@ theorem sigma_rw_simp {S T : Type _} {m m' : Σ (y : Type _), S → y → T → 
   m.snd x v y ↔ m'.snd x ((cast_first h).mp v) y := by
   constructor <;> (intros; subst h; assumption)
 
--- TODO: Can I write differently the lambda that extract the element from p's queue
-def φ (rhs : rhsModuleType T₁ T₂ T₃) (lhs : lhsModuleType T₁ T₂ T₃) : Prop :=
+inductive partially
+
+#reduce (lhsModule T₁ T₂ T₃)
+
+inductive partially_flushed: lhsModuleType T₁ T₂ T₃ -> Prop where
+| lhs: ∀ lower arb, partially_flushed ⟨lower, ⟨ [], arb ⟩ ⟩
+| rhs: ∀ lower arb, partially_flushed ⟨lower, ⟨ arb, [] ⟩ ⟩
+
+def ψ (rhs : rhsModuleType T₁ T₂ T₃) (lhs : lhsModuleType T₁ T₂ T₃) : Prop :=
   let ⟨⟨j2l, j2r⟩, ⟨j1l, j1r⟩⟩ := lhs
   let ⟨⟨j2l', j2r'⟩, ⟨⟨j1l', j1r'⟩, p⟩⟩ := rhs
-  (j1l ++ j2l.map Prod.fst = j2l' ++ p.map (fun ⟨⟨elem, _⟩, _⟩ => elem)) ∧
-  (j1r ++ j2l.map Prod.snd = j1l' ++ j2r'.map Prod.fst ++ p.map (fun ⟨⟨_, elem⟩, _⟩ => elem)) ∧
-  (j2r = j1r' ++ j2r'.map Prod.snd ++ p.map (fun ⟨⟨_, _⟩, elem⟩ => elem))
+  (j2l.map Prod.fst ++ j1l = p.map (Prod.fst ∘ Prod.fst) ++ j2l') ∧
+  (j2l.map Prod.snd ++ j1r = p.map ((Prod.snd ∘ Prod.fst)) ++ j2r'.map Prod.fst ++ j1l') ∧
+  (j2r = p.map Prod.snd ++ j2r'.map Prod.snd ++ j1r')
 
+
+-- TODO: Can I write differently the lambda that extract the element from p's queue
+def φ (rhs : rhsModuleType T₁ T₂ T₃) (lhs : lhsModuleType T₁ T₂ T₃) : Prop :=
+  (ψ rhs lhs) ∧ (partially_flushed lhs)
+
+-- loogle.lean-lang.org
 theorem φ_indistinguishable :
-  ∀ x y, φ x y → Module.indistinguishable (rhsModule T₁ T₂ T₃) (lhsModule T₁ T₂ T₃) x y := by sorry
+  ∀ x y, φ x y → Module.indistinguishable (rhsModule T₁ T₂ T₃) (lhsModule T₁ T₂ T₃) x y := by
+  intro ⟨⟨_, _⟩, ⟨⟨_, _⟩, _⟩⟩ ⟨⟨_, _⟩, ⟨_, _⟩⟩ Hφ
+  constructor <;> intro ident ⟨⟨_, _⟩, ⟨⟨_, _⟩, _⟩⟩ v H
+  . by_cases HContains: ((rhsModule T₁ T₂ T₃).inputs.contains ident)
+    . unfold rhsModule lhsModule at *; simp at v H HContains; simp
+      rcases HContains with h | h | h
+      all_goals
+        subst ident
+        rw [PortMap.rw_rule_execution] at *
+        apply Exists.intro ⟨ ⟨ _, _ ⟩, _, _ ⟩
+        rw [PortMap.rw_rule_execution]
+        unfold φ ψ at Hφ <;> simp at Hφ
+        dsimp
+        and_intros <;> rfl
+    . exfalso; exact (PortMap.getIO_not_contained_false HContains H)
+  . by_cases HContains: ((rhsModule T₁ T₂ T₃).outputs.contains ident)
+    . unfold rhsModule lhsModule at *; simp at v H HContains; simp
+      subst ident
+      rw [PortMap.rw_rule_execution] at *
+      simp at H
+      repeat cases ‹_ ∧ _›
+      subst_vars
+      cases ‹partially_flushed _› <;> simp at *
+      . rename_i left
+        rw [List.map_eq_cons_iff] at left
+        obtain ⟨ ⟨v'1, v'2⟩, j2lr, h1, h2, h3⟩ := left
+        subst_vars
+        obtain ⟨⟨v111, v112⟩, v12⟩ := v
+        dsimp at *
+        rename_i left
+        rw [List.cons.injEq] at left
+        repeat cases left
+        subst_vars
+        apply Exists.intro ⟨ ⟨ _, _ ⟩, _, _ ⟩
+        rw [PortMap.rw_rule_execution]
+        dsimp
+        and_intros <;> try rfl
+      . rename_i left
+        rw [List.map_eq_cons_iff] at left
+        obtain ⟨ ⟨v'1, v'2⟩, j2lr, h1, h2, h3⟩ := left
+        subst_vars
+        obtain ⟨⟨v111, v112⟩, v12⟩ := v
+        dsimp at *
+        rename_i left
+        rw [List.cons.injEq] at left
+        repeat cases left
+        subst_vars
+        apply Exists.intro ⟨ ⟨ _, _ ⟩, _, _ ⟩
+        rw [PortMap.rw_rule_execution]
+        dsimp
+        and_intros <;> try rfl
+    . exfalso; exact (PortMap.getIO_not_contained_false HContains H)
 
-theorem refines {T: Type _} [DecidableEq T]: rhsModule T₁ T₂ T₃ ⊑_{φ} lhsModule T₁ T₂ T₃ := by sorry
+theorem something':
+  ∀ s, ∃ s', existSR (lhsModule T₁ T₂ T₃).internals s s' ∧ partially_flushed s' := by
+  intro ⟨⟨l1, l2⟩, ⟨l3, l4⟩⟩
+  induction l3 generalizing l1 l2 l4 with
+  | nil =>
+    apply Exists.intro
+    and_intros
+    . apply existSR_reflexive
+    . constructor
+  | cons x xs ih =>
+    cases l4
+    . apply Exists.intro
+      and_intros
+      . apply existSR_reflexive
+      . constructor
+    . rename_i head tail
+      specialize ih (l1 ++ [(x, head)]) l2 tail
+      obtain ⟨ ⟨⟨_, _⟩, ⟨_, _⟩⟩, HExists, HPartiallyFlushed⟩ := ih
+      apply Exists.intro ⟨ ⟨ _, _ ⟩, _, _ ⟩
+      and_intros
+      . apply existSR.step _ ⟨ ⟨ _, _ ⟩, _, _ ⟩ _
+        . unfold lhsModule; simp
+          rfl
+        . repeat apply Exists.intro
+          and_intros <;> rfl
+        . apply HExists
+      . assumption
+
+theorem something:
+  ∀ i s s', ψ i s → existSR (lhsModule T₁ T₂ T₃).internals s s' → ψ i s' := by
+  intro ⟨⟨_, _⟩, ⟨⟨_, _⟩, _⟩⟩ s s' Hψ E
+  induction E
+  . assumption
+  . rename_i init mid _ rule Hrule c _ Himpl
+    apply Himpl; clear Himpl
+    unfold lhsModule at Hrule; simp at Hrule
+    subst_vars
+    obtain ⟨_, _, _, _, _, _, _, _⟩ := c
+    let ⟨⟨_, _⟩, ⟨_, _⟩⟩ := init
+    let ⟨⟨_, _⟩, ⟨_, _⟩⟩ := mid
+    rename_i a _ _ _ _ _ b; simp at a b
+    obtain ⟨ ⟨_, _⟩, ⟨_, _⟩⟩ := a
+    obtain ⟨ ⟨_, _⟩ , ⟨_, _⟩⟩ := b
+    unfold ψ at *; simp at *
+    subst_vars
+    obtain ⟨ _, ⟨_, _⟩ ⟩ := Hψ
+    simp; and_intros <;> assumption
+
+theorem something'':
+  ∀ i i' s, ψ i s → existSR (rhsModule T₁ T₂ T₃).internals i i' → ψ i' s := by
+  intro i i' ⟨⟨_, _⟩, ⟨_, _⟩⟩ Hψ E
+  induction E
+  . assumption
+  . rename_i init mid _ rule Hrule c _ Himpl
+    apply Himpl; clear Himpl
+    unfold rhsModule at Hrule; simp at Hrule
+    cases Hrule <;> subst_vars
+    . obtain ⟨_, _, _, _, _, _, _, ⟨⟨⟨_, _⟩, _⟩, _⟩, ⟨_, _⟩, _⟩ := c
+      let ⟨⟨_, _⟩, ⟨_, _⟩⟩ := init
+      let ⟨⟨_, _⟩, ⟨_, _⟩⟩ := mid
+      unfold ψ at *; simp at *
+      rename_i synth1 synth2;
+      obtain ⟨_, _⟩ := synth1
+      obtain ⟨_, _⟩ := synth2
+      obtain ⟨_, _, _⟩ := Hψ
+      and_intros <;> subst_vars <;> try simp
+      . assumption
+      . rename_i synth1 _ _ _ _ _ _
+        rw [<- synth1]; subst_vars
+        assumption
+      . assumption
+    . obtain ⟨_, _, _, _, _, _, _, _, ⟨⟨⟨_, _⟩, _⟩, ⟨⟨_, _⟩, _⟩⟩⟩ := c
+      let ⟨⟨_, _⟩, ⟨_, _⟩⟩ := init
+      let ⟨⟨_, _⟩, ⟨_, _⟩⟩ := mid
+      unfold ψ at *; simp at *
+      rename_i synth1 synth2;
+      obtain ⟨_, _⟩ := synth1
+      obtain ⟨_, _⟩ := synth2
+      obtain ⟨_, _, _⟩ := Hψ
+      and_intros <;> subst_vars <;> simp
+      . assumption
+      . assumption
+
+theorem refines {T: Type _} [DecidableEq T]: rhsModule T₁ T₂ T₃ ⊑_{φ} lhsModule T₁ T₂ T₃ := by
+  unfold Module.refines_φ
+  intro init_i init_s Hφ
+  apply Module.comp_refines.mk
+  -- input rules
+  . intro ident i s a
+    by_cases HContains: ((rhsModule T₁ T₂ T₃).inputs.contains ident)
+    . unfold rhsModule at HContains; simp at HContains
+      rcases HContains with h | h | h <;> subst_vars <;> simp
+      . rw [PortMap.rw_rule_execution] at a
+
+
+        unfold rhsModule at s; simp at s
+
+        unfold rhsModule lhsModule at *
+        subst_vars; simp at *
+        rw [PortMap.getIO] at *
+        apply Exists.intro
+        rw [PortMap.rw_rule_execution] at *
+        simp at *
+        and_intros
+        . obtain ⟨ ⟨_, _⟩ , _⟩ := a
+          subst_vars
+          sorry
+        . rfl
+        . sorry
+        sorry
+      . sorry
+      . sorry
+    . exfalso; exact (PortMap.getIO_not_contained_false HContains a)
+  -- output rules
+  . intros ident mid_i v rhs
+    by_cases HContains: ((rhsModule T₁ T₂ T₃).outputs.contains ident)
+    . unfold rhsModule at HContains; simp at HContains; subst_vars
+      rw [PortMap.rw_rule_execution] at rhs; simp at rhs
+      obtain ⟨⟨_, _⟩, _⟩ := v
+      obtain ⟨⟨_, _⟩, ⟨⟨_, _⟩, _⟩⟩ := init_i
+      obtain ⟨⟨_, _⟩, ⟨_, _⟩⟩ := init_s
+      obtain ⟨⟨_, _⟩, ⟨⟨_, _⟩, _⟩⟩ := mid_i
+      obtain ⟨⟨_, ⟨_, _⟩⟩, ⟨_, _⟩⟩ := rhs
+      unfold φ at Hφ <;> obtain ⟨Hψ, pf⟩ := Hφ
+      unfold ψ at Hψ; simp at Hψ
+      obtain ⟨_, ⟨_, _⟩⟩ := Hψ
+      simp at *; subst_vars
+      cases pf <;> simp
+      . apply Exists.intro ⟨⟨_, _⟩, ⟨_, _⟩⟩
+        apply And.intro
+        . unfold lhsModule; simp; rw [PortMap.rw_rule_execution] <;> sorry
+        . sorry
+      . sorry
+    . exfalso; exact (PortMap.getIO_not_contained_false HContains rhs)
+  -- internal rules
+  . intros rule mid_i HruleIn Hrule
+    unfold φ at Hφ <;> obtain ⟨ Hψ, _ ⟩ := Hφ
+    use init_s
+    apply And.intro
+    . exact existSR_reflexive
+    . unfold φ <;> apply And.intro
+      . unfold rhsModule at HruleIn
+        simp at HruleIn
+        obtain ⟨_, _⟩ := HruleIn <;> subst_vars
+        . obtain⟨_, _, _, _, _, _, _, ⟨⟨⟨_, _⟩, _⟩, _⟩, ⟨⟨_, _⟩, _⟩⟩ := Hrule
+          obtain ⟨⟨_, _⟩, ⟨⟨_, _⟩, _⟩⟩ := init_i
+          obtain ⟨⟨_, _⟩, ⟨⟨_, _⟩⟩⟩ := init_s
+          . subst_vars
+            obtain ⟨⟨_, _⟩, ⟨⟨_, _⟩, _⟩⟩ := mid_i
+            unfold ψ at *; simp at *
+            and_intros
+            . rename_i synth1 synth2
+              obtain ⟨_, _⟩ := synth1
+              obtain ⟨_, _⟩ := synth2
+              obtain ⟨_, ⟨_, _⟩⟩ := Hψ
+              subst_vars
+              assumption
+            . rename_i synth1 synth2
+              obtain ⟨_, _⟩ := synth1
+              obtain ⟨⟨_, _⟩, _⟩ := synth2
+              obtain ⟨_, ⟨_, _⟩⟩ := Hψ
+              subst_vars <;> simp
+              assumption
+            . rename_i synth1 synth2
+              obtain ⟨_, _⟩ := synth1
+              obtain ⟨⟨_, _⟩, _⟩ := synth2
+              obtain ⟨_, ⟨_, _⟩⟩ := Hψ
+              subst_vars <;> simp
+          . subst_vars
+            obtain ⟨⟨_, _⟩, ⟨⟨_, _⟩, _⟩⟩ := mid_i
+            unfold ψ at *; simp at *
+            and_intros
+            . rename_i synth1 synth2
+              obtain ⟨_, _⟩ := synth1
+              obtain ⟨⟨_, _⟩, _⟩ := synth2
+              obtain ⟨_, ⟨_, _⟩⟩ := Hψ
+              subst_vars <;> assumption
+            . rename_i synth1 synth2
+              obtain ⟨_, _⟩ := synth1
+              obtain ⟨⟨_, _⟩, _⟩ := synth2
+              obtain ⟨_, ⟨_, _⟩⟩ := Hψ
+              subst_vars <;> simp
+              assumption
+            . rename_i synth1 synth2
+              obtain ⟨_, _⟩ := synth1
+              obtain ⟨⟨_, _⟩, _⟩ := synth2
+              obtain ⟨_, ⟨_, _⟩⟩ := Hψ
+              subst_vars <;> simp
+        . obtain⟨_, _, _, _, _, _, _, _, ⟨⟨⟨_, _⟩, _⟩, ⟨⟨_, _⟩, _⟩⟩⟩ := Hrule
+          obtain ⟨⟨_, _⟩, ⟨⟨_, _⟩, _⟩⟩ := init_i
+          obtain ⟨⟨_, _⟩, ⟨⟨_, _⟩⟩⟩ := init_s
+          . subst_vars
+            obtain ⟨⟨_, _⟩, ⟨⟨_, _⟩, _⟩⟩ := mid_i
+            unfold ψ at *; simp at *
+            and_intros
+            . rename_i synth1 synth2 synth3
+              obtain ⟨_, _⟩ := synth1
+              obtain ⟨_, _⟩ := synth2
+              obtain ⟨⟨_, _⟩, _⟩ := synth3
+              obtain ⟨_, ⟨_, _⟩⟩ := Hψ
+              subst_vars <;> simp
+              assumption
+            . rename_i synth1 synth2 synth3
+              obtain ⟨⟨_, _⟩, _⟩ := synth1
+              obtain ⟨_, _⟩ := synth2
+              obtain ⟨⟨_, _⟩, _⟩ := synth3
+              obtain ⟨_, ⟨_, _⟩⟩ := Hψ
+              subst_vars <;> simp
+              assumption
+            . rename_i synth1 synth2 synth3
+              obtain ⟨⟨_, _⟩, _⟩ := synth1
+              obtain ⟨_, _⟩ := synth2
+              obtain ⟨⟨_, _⟩, _⟩ := synth3
+              obtain ⟨_, ⟨_, _⟩⟩ := Hψ
+              subst_vars <;> simp
+          . subst_vars
+            obtain ⟨⟨_, _⟩, ⟨⟨_, _⟩, _⟩⟩ := mid_i
+            unfold ψ at *; simp at *
+            and_intros
+            . rename_i synth1 synth2 synth3
+              obtain ⟨_, _⟩ := synth1
+              obtain ⟨_, _⟩ := synth2
+              obtain ⟨⟨_, _⟩, _⟩ := synth3
+              obtain ⟨_, ⟨_, _⟩⟩ := Hψ
+              subst_vars <;> simp
+              assumption
+            . rename_i synth1 synth2 synth3
+              obtain ⟨⟨_, _⟩, _⟩ := synth1
+              obtain ⟨_, _⟩ := synth2
+              obtain ⟨⟨_, _⟩, _⟩ := synth3
+              obtain ⟨_, ⟨_, _⟩⟩ := Hψ
+              subst_vars <;> simp
+              assumption
+            . rename_i synth1 synth2 synth3
+              obtain ⟨⟨_, _⟩, _⟩ := synth1
+              obtain ⟨_, _⟩ := synth2
+              obtain ⟨⟨_, _⟩, _⟩ := synth3
+              obtain ⟨_, ⟨_, _⟩⟩ := Hψ
+              subst_vars <;> simp
+      . assumption
+
 
 end DataflowRewriter.JoinRewrite
